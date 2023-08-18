a85b74acf8a3fec5e6c06a3429340ecb2bc1376b
destroyedlolo <l_faillie@yahoo.com>
2023-08-19 01:04:38 +0200

    Start with config file reading

--- src/TeleInfod.c
+++ src/TeleInfod.c
@@ -20,20 +20,126 @@
 #include <unistd.h>
 #include <string.h>
 
+#ifdef USE_MOSQUITTO
+#	include <mosquitto.h>
+#elif defined(USE_PAHO)
+#	include <MQTTClient.h>
+#endif
+
 #include "Version.h"
 #include "Config.h"
 
 unsigned int debug = 0;
+const char *Broker_Host;
+int Broker_Port;
 
-void read_configuration( const char *fch){
+#ifdef USE_MOSQUITTO
+struct mosquitto *mosq;
+#elif defined(USE_PAHO)
+MQTTClient client;
+#else
+#	error "No MQTT library defined"
+#endif
+
+	/* **
+	 * Helpers
+	 * **/
+char *removeLF(char *s){
+	size_t l=strlen(s);
+	if(l && s[--l] == '\n')
+		s[l] = 0;
+	return s;
+}
+
+char *striKWcmp( char *s, const char *kw ){
+/* compares string s against kw
+ * Return :
+ * 	- remaining string if the keyword matches
+ * 	- NULL if the keyword is not found
+ */
+	size_t klen = strlen(kw);
+	if( strncasecmp(s,kw,klen) )
+		return NULL;
+	else
+		return s+klen;
+}
+
+char *extr_arg(char *s, int l){ 
+/* Extracts an argument from TéléInfo trame 
+ *	s : argument string just after the token
+ *	l : length of the argument
+ */
+	s++;	/* Skip the leading space */
+	s[l]=0;
+	return s;
+}
+
+
+	/* **
+	 * Fill configuration from given configuration file
+	 * -> fch : configuration file to read
+	 * **/
+static void read_configuration( const char *fch){
 	FILE *f;
 	char l[MAXLINE];
 	char *arg;
+	unsigned int ln = 0;
+
+		/* default configuration */
+#ifdef USE_PAHO
+	Broker_Host = "tcp://localhost:1883";
+	client = NULL;
+#else
+	Broker_Host = "localhost";
+	Broker_Port = 1883;
+	mosq = NULL;
+#endif
+
+	if(debug)
+		printf("Reading configuration file '%s'\n", fch);
+
+		/* Reading the configuration file */
+	if(!(f=fopen(fch, "r"))){
+		perror(fch);
+		exit(EXIT_FAILURE);
+	}
+
+	while(fgets(l, MAXLINE, f)){
+		ln++;
+
+		if(*l == '#' || *l == '\n')	/* Ignore comments */
+			continue;
+
+		if((arg = striKWcmp(l,"Broker_Host="))){
+			Broker_Host = removeLF(arg);
+			if(debug)
+				printf("\tBroker host : '%s'\n", Broker_Host);
+		} else if((arg = striKWcmp(l,"Broker_Port="))){
+#if defined(USE_PAHO)
+			fprintf(stderr, "\nERROR line %u : When using Paho library, Broker_Port directive is not used.\n"
+				"Instead, use\n"
+				"\tBroker_Host=protocol://host:port\n", ln);
+			exit(EXIT_FAILURE);
+#else
+			Broker_Port = atoi( arg );
+			if(debug)
+				printf("Broker port : %d\n", Broker_Port);
+#endif
+		} else {
+			fprintf(stderr, "\nERROR line %u : \"%s\" is not a known configuration directive\n", ln, removeLF(l));
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if(debug)
+		puts("");
+
+	fclose(f);
 }
 
 int main(int ac, char **av){
 	const char *conf_file = DEFAULT_CONFIGURATION_FILE;
-
+	
 		/* reading arguments */
 	int opt;
 	while((opt = getopt(ac, av, "hdDf:")) != -1){
@@ -43,7 +149,9 @@ int main(int ac, char **av){
 		case 'd':
 			debug += 1;
 			printf("TeleInfod (%s) %s\n", VERSION, COPYRIGHT);
-			puts("Starting ...");
+			break;
+		case 'f':
+			conf_file = optarg;
 			break;
 		case 'h':
 			fprintf(stderr, "TeleInfod (%s) %s\n"
@@ -57,12 +165,11 @@ int main(int ac, char **av){
 				VERSION, COPYRIGHT, DEFAULT_CONFIGURATION_FILE
 			);
 			exit(EXIT_FAILURE);
-		case 'f':
-			conf_file = optarg;
-			break;
 		}
 	}
 
 	read_configuration( conf_file );
 
+	if(debug)
+		puts("Starting ...");
 }
